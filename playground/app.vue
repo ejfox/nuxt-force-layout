c0d9fe9e8183807e46df91ce4031aec454097df0
EJ Fox <ejfox@ejfox.com>
2023-11-02 12:29:36 -0400

    init: documentation and plan for module

---
+++ playground/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Nuxt module playground!
+    <ForceLayout :edges="edges" :nodes="nodes" />
+  </div>
+</template>
+
+<script setup>
+const { data: nodes } = await useAsyncData('nodes', () => fetch('/api/nodes').then(res => res.json()))
+
+const { data: edges } = await useAsyncData('edges', () => fetch('/api/edges').then(res => res.json()))
+</script>
---
+++ src/runtime/components/ForceLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1>A force layout would go here! Weeeee!</h1>
+</template>
+<script setup>
+
+</script>
